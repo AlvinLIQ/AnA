69b8e46a88e56eb5c4abd300df9047d939034b65
AlvinLIQ <AlvinLIQ@outlook.com>
2023-01-25 02:09:18 -0800

    Add Windows Build Option

--- Src/Core/AnA_App.cpp
+++ Src/Core/AnA_App.cpp
@@ -124,7 +124,7 @@ void AnA_App::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageI
     renderPassInfo.renderArea.offset = {0, 0};
     renderPassInfo.renderArea.extent = swapChainExtent;
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+    clearValues[0].color = {{0.1f, 0.1f, 0.1f, 1.0f}};
     clearValues[1].depthStencil = {1.0f, 0};
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
