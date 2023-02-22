334da73c026afbcb8e6b2ce78a7cd7ec163c50df
AlvinLIQ <alvinliq@outlook.com>
2023-02-23 00:09:57 +1300

    add input manager

--- Src/Core/Input/AnA_InputManager.cpp
+++ Src/Core/Input/AnA_InputManager.cpp
@@ -1,1 +1,36 @@
 #include "Headers/AnA_InputManager.hpp"
+#include <GLFW/glfw3.h>
+
+using namespace AnA;
+using namespace AnA::Input;
+
+AnA_InputManager *_aInputManager;
+
+AnA_InputManager::AnA_InputManager(AnA_Window *&mWindow) : aWindow {mWindow}
+{
+    _aInputManager = this;
+/*
+    auto &window = aWindow->GetGLFWwindow();
+    glfwSetKeyCallback(window, AnA_InputManager::keyCallback);*/
+}
+
+void AnA_InputManager::CheckAndRunCallbacks()
+{
+    auto &window = aWindow->GetGLFWwindow();
+    auto &keyMapConfigs = _aInputManager->GetKeyMapConfigs();
+    for (auto &keyMapConfig : keyMapConfigs)
+    {
+        if (keyMapConfig.callBack != nullptr && glfwGetKey(window, keyMapConfig.keyCode) == GLFW_PRESS)
+            keyMapConfig.callBack();
+    }
+}
+
+void AnA_InputManager::keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    auto &keyMapConfigs = _aInputManager->GetKeyMapConfigs();
+    for (auto &keyMapConfig : keyMapConfigs)
+    {
+        if (keyMapConfig.keyCode == key && keyMapConfig.callBack != nullptr)
+            keyMapConfig.callBack();
+    }
+}
--- Src/Core/Input/Headers/AnA_InputManager.hpp
+++ Src/Core/Input/Headers/AnA_InputManager.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include "../../Headers/AnA_Window.hpp"
+#include <vector>
+
+namespace AnA
+{
+    namespace Input
+    {
+        class AnA_InputManager
+        {
+        public:
+            struct KeyMapConfig
+            {
+                void(*callBack)();
+                int keyCode;
+                int action;
+            };
+            AnA_InputManager(AnA_Window *&mWindow);
+
+            const std::vector<KeyMapConfig> &GetKeyMapConfigs() const
+            {
+                return keyMapConfigs;
+            }
+            void CheckAndRunCallbacks();
+        private:
+            AnA_Window *&aWindow;
+            std::vector<KeyMapConfig> keyMapConfigs;
+            static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
+        };
+    }
+}
