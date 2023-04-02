cd3832c2e7be6541772fd9cabbf4d5c1904441b9
AlvinLIQ <alvinliq@outlook.com>
2023-04-02 22:20:08 +1200

    LIGHTING

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-glslc Src/Core/ShaderCodes/AnA_2D.frag -o Shaders/frag.spv
-glslc Src/Core/ShaderCodes/AnA_2D.vert -o Shaders/vert.spv
+glslc Src/Core/ShaderCodes/AnA_Basic.frag -o Shaders/frag.spv
+glslc Src/Core/ShaderCodes/AnA_Basic.vert -o Shaders/vert.spv
