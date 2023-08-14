050e945b53ded1fb220fe34a4d1a9779508b19b2
AlvinLIQ <alvinliq@outlook.com>
2023-08-14 22:43:24 +1200

    init compute shader

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@ glslc Src/Core/ShaderCodes/Basic.frag -o Shaders/frag.spv
 glslc Src/Core/ShaderCodes/Basic.vert -o Shaders/vert.spv
 glslc Src/Core/ShaderCodes/PointLight.frag -o Shaders/pointLightFrag.spv
 glslc Src/Core/ShaderCodes/PointLight.vert -o Shaders/pointLightVert.spv
+glslc Src/Core/ShaderCodes/CollisionDetect.comp -o Shaders/compute.spv
