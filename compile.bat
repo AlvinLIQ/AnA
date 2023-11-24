0888ddeb7a9eae794e92874991428f86790b2597
AlvinLIQ <alvinliq@outlook.com>
2023-11-25 04:06:39 +1300

    Updated compile.bat for windows

--- compile.bat
+++ compile.bat
@@ -3,3 +3,5 @@ glslc Src/Core/ShaderCodes/Basic.vert -o Shaders/vert.spv
 glslc Src/Core/ShaderCodes/PointLight.frag -o Shaders/pointLightFrag.spv
 glslc Src/Core/ShaderCodes/PointLight.vert -o Shaders/pointLightVert.spv
 glslc Src/Core/ShaderCodes/CollisionDetect.comp -o Shaders/compute.spv
+glslc Src/Core/ShaderCodes/Line.vert -o Shaders/lineVert.spv
+glslc Src/Core/ShaderCodes/Line.frag -o Shaders/lineFrag.spv
