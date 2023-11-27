2445878e529e6891d33f054f922f438682c626ff
AlvinLIQ <alvinliq@outlook.com>
2023-11-27 16:26:02 +1300

    Move some constants out of the loop

--- Src/Core/Renderer.cpp
+++ Src/Core/Renderer.cpp
@@ -2,7 +2,6 @@
 #include "Headers/SwapChain.hpp"
 
 #include <cassert>
-#include <array>
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <vulkan/vulkan_core.h>
@@ -141,11 +140,8 @@ void Renderer::BeginSwapChainRenderPass(VkCommandBuffer commandBuffer)
     renderPassInfo.framebuffer = aSwapChain->GetSwapChainFramebuffers()[currentImageIndex];
     renderPassInfo.renderArea.offset = {0, 0};
     renderPassInfo.renderArea.extent = swapChainExtent;
-    std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = {{0.2f, 0.2f, 0.2f, 1.0f}};
-    clearValues[1].depthStencil = {1.0f, 0};
-    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
-    renderPassInfo.pClearValues = clearValues.data();
+    renderPassInfo.clearValueCount = numsof(clearValues);
+    renderPassInfo.pClearValues = clearValues;
     vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_SECONDARY_COMMAND_BUFFERS);
 }
 
