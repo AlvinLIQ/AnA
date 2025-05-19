5a29595732c6c6aed8f845f0994f02ce7e96cd53
AlvinLIQ <alvinliq@outlook.com>
2025-05-19 23:56:38 +0800

    Init CSM Mesh shader

--- Src/Core/ShaderCodes/mesh.h
+++ Src/Core/ShaderCodes/mesh.h
@@ -4,3 +4,20 @@ struct Vertex
     vec3 normal;
     vec2 uv;
 };
+
+bool isSphereInsideFrustum(in vec3 center, in float radius, in vec4 frustumPlanes[6])
+{
+    for (int i = 0; i < 6; ++i)
+    {
+        vec4 plane = frustumPlanes[i];
+        float d = dot(plane.xyz, center) + plane.w + radius;
+        if (d < 0)
+            return false;
+    }
+    return true;
+}
+
+bool isMeshletFacingCamera(in vec3 cameraPosition, in vec3 coneApex, in vec3 coneAxis, in float cutoff)
+{
+    return dot(normalize(coneApex - cameraPosition), coneAxis) <= cutoff;
+}
