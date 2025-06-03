e649140a2912f10227877d106ce277b0a0ca4458
AlvinLIQ <alvinliq@outlook.com>
2025-06-03 08:02:16 -0700

    Add per primitive culling to mesh shader

--- Src/Core/Device.cpp
+++ Src/Core/Device.cpp
@@ -756,7 +756,7 @@ VkQueue &Device::GetPresentQueue()
 
 VkSampleCountFlagBits Device::GetMaxUsableSampleCount()
 {
-    return usableSamples.size() >= 2 ? *(usableSamples.end() - 2) : usableSamples.front();
+    return usableSamples.size() > 2 ? usableSamples[1] : usableSamples.front();
 }
 
 Device::QueueFamilyIndices Device::FindQueueFamilies(VkPhysicalDevice device)
