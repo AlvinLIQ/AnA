23732f83c6c4b1bbd356c25a37c40f91df6d3427
AlvinLIQ <alvinliq@outlook.com>
2025-10-30 19:29:32 +0000

    Fix index missing

--- Src/Core/Resources/Headers/Model.hpp
+++ Src/Core/Resources/Headers/Model.hpp
@@ -44,8 +44,8 @@ namespace AnA
 
         struct Meshlet
         {
-            uint32_t vertices[128];
-            uint8_t indices[256 * 3];
+            uint32_t vertices[64];
+            uint8_t indices[128 * 3];
             uint32_t indexCount;
             uint32_t vertexCount;
             uint32_t farVertexID;
--- Src/Core/ShaderCodes/mesh.h
+++ Src/Core/ShaderCodes/mesh.h
@@ -1,6 +1,6 @@
 
-#define MAX_VERTICES 128
-#define MAX_PRIMITIVES 256
+#define MAX_VERTICES 64
+#define MAX_PRIMITIVES 128
 #define TASK_WORKGROUP 64
 
 const uint bytesToBits[4] = {0u, 8u, 16u, 24u};
