3ff5dac2c256c6e4886c2e35582519a6556162a0
Alvin <alvinliq@outlook.com>
2025-11-20 16:42:12 +0800

    Enable Mesh Shader Text Rendering

--- Src/Core/App.cpp
+++ Src/Core/App.cpp
@@ -272,13 +272,13 @@ void App::onCommandBufferRecording(CommandBuffer& commandBuffer)
     aRenderSystem.RenderIndirect(commandBuffer, aResourceManager.MainScene,
         aDevice.MeshShaderSupport() ? aResourceManager.Shaders[5] : aResourceManager.Shaders[0],
         swapChain.CurrentFrame);
-/*
+
     if (aDevice.MeshShaderSupport())
     {
         swapChain.SetViewport(commandBuffer);
         aRenderSystem.RenderIndirect(commandBuffer, aResourceManager.TextContext, 
             aResourceManager.Shaders[6], swapChain.CurrentFrame);
-    }*/
+    }
     swapChain.SetViewport(commandBuffer, aResourceManager.Shapes.Extent);
     aRenderSystem.RenderIndirect(commandBuffer, aResourceManager.Shapes, 
         aResourceManager.Shaders[1], swapChain.CurrentFrame);
