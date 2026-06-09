e1582b01dbe4bdc7e1d70dfa0ce886a4ddca78c8
AlvinLIQ <alvinliq@outlook.com>
2026-06-09 17:31:50 +0800

    add cflags to GUI libs

--- Src/GUI/Controls/Styles/Default/ControlStyle.hpp
+++ Src/GUI/Controls/Styles/Default/ControlStyle.hpp
@@ -20,9 +20,6 @@ namespace AnA
     const Vec2 ButtonMinSize = {0.1f, 0.06f};
     const AlignType ButtonRenderMode = Auto;*/
     const Vec2 ButtonMinAbsoluteSize = {80.f, 36.0f};
-#ifndef ANA_THEME
-#define ANA_THEME 0
-#endif
 #if ANA_THEME==0
     const glm::vec4 ButtonBackgroundColor = {0.2f, 0.2f, 0.2f, 1.0f};
     const glm::vec4 ButtonPointerMovedBackgroundColor = {0.22f, 0.22, 0.22f, 1.0f};
