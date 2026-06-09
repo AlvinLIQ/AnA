c873f34afd6acab08048d75c1875857fb3b30309
AlvinLIQ <alvinliq@outlook.com>
2026-06-09 17:11:41 +0800

    add theme option

--- Src/GUI/Controls/Styles/Default/ControlStyle.hpp
+++ Src/GUI/Controls/Styles/Default/ControlStyle.hpp
@@ -13,13 +13,23 @@ namespace AnA
     const Vec2 ControlMinSize = {};
 
     const AlignType ControlRenderMode = Auto;
+    const glm::vec4 MaxColor = {1.0f, 1.0f, 1.0f, 2.0f};
     /*Button*/
 
     /*
     const Vec2 ButtonMinSize = {0.1f, 0.06f};
     const AlignType ButtonRenderMode = Auto;*/
     const Vec2 ButtonMinAbsoluteSize = {80.f, 36.0f};
+#ifndef ANA_THEME
+#define ANA_THEME 0
+#endif
+#if ANA_THEME==0
     const glm::vec4 ButtonBackgroundColor = {0.2f, 0.2f, 0.2f, 1.0f};
     const glm::vec4 ButtonPointerMovedBackgroundColor = {0.22f, 0.22, 0.22f, 1.0f};
     const glm::vec4 ButtonPointerPressedBackgroundColor = {0.18f, 0.18f, 0.18f, 1.0f};
+#elif ANA_THEME==1
+    const glm::vec4 ButtonBackgroundColor = {0.8f, 0.8f, 0.8f, 1.0f};
+    const glm::vec4 ButtonPointerMovedBackgroundColor = {0.7f, 0.7f, 0.7f, 1.0f};
+    const glm::vec4 ButtonPointerPressedBackgroundColor = {0.6f, 0.6f, 0.6f, 1.0f};
+#endif
 }
