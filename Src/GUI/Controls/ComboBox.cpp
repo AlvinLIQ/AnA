eaf8cb88ce6d40222c4a2dc1bdb9dac1eb5eb8dc
AlvinLIQ <alvinliq@outlook.com>
2026-06-09 22:32:21 +0000

    init combobox

---
+++ Src/GUI/Controls/ComboBox.cpp
@@ -0,0 +1,14 @@
+#include "Headers/ComboBox.hpp"
+
+using namespace AnA;
+using namespace Controls;
+
+ComboBox::ComboBox()
+{
+
+}
+
+ComboBox::~ComboBox()
+{
+
+}
---
+++ Src/GUI/Controls/Headers/ComboBox.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "TextBlock.hpp"
+
+namespace AnA
+{
+    namespace Controls
+    {
+        class ComboBox : public TextBlock
+        {
+        public:
+            ComboBox();
+            ~ComboBox();
+        private:
+        };
+    }
+}
